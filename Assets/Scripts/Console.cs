e21e7242d4e62a8122cace72cd9adfdcfd08e496
Maxim360 <33699113+Maxim360Squad@users.noreply.github.com>
2021-07-10 02:22:31 +0300

    Fix and Update Console // Add new command to console

--- Assets/Scripts/Console.cs
+++ Assets/Scripts/Console.cs
@@ -8,9 +8,10 @@ using UnityEngine.UI;
 public class Console : MonoBehaviour
 {
     public GameObject ConsoleCanvas;
+    public GameObject FonForConsole;
     public TMP_InputField InputField;
     public TMP_Text OutputText;
-    public Button SendBtn;
+    public TMP_Text OutputPageText;
 
     public Client Network;
 
@@ -25,28 +26,63 @@ public class Console : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.BackQuote) && !_isConsoleOpen)
         {
-            ConsoleCanvas.active = _isConsoleOpen = true;
+            ConsoleCanvas.active = FonForConsole.active =  _isConsoleOpen = true;
             Cursor.lockState = CursorLockMode.None;
+
+            //Makes the Input Field the selected UI Element.
+            InputField.ActivateInputField();
+            InputField.Select();
         }
         else if (Input.GetKeyDown(KeyCode.BackQuote) && _isConsoleOpen)
         {
-            ConsoleCanvas.active = _isConsoleOpen = false;
+            ConsoleCanvas.active = FonForConsole.active = _isConsoleOpen = false;
             Cursor.lockState = CursorLockMode.Locked;
         }
 
-        string[] SplitText;
-
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            SplitText = InputField.text.Split(new string[] { " ", ":" }, StringSplitOptions.None);
+            MessageSend();
+        }
+    }
+    public void MessageSend()
+    {
+        //\n\r + \n\r  - for enter
+        OutputText.text += $"\n\r\n\r[{DateTime.UtcNow}] [User/{Network.PlayerName}] {InputField.text}\n\r";
 
-            switch (SplitText[0])
-            {
-                case "connect":
+        string CommandNameText = InputField.text.Split(new string[] { " " }, StringSplitOptions.None)[0];
+        CommandsExecuting(CommandNameText, InputField.text);
+
+        InputField.text = ""; //Clear Input Text
+
+        //Makes the Input Field the selected UI Element.
+        InputField.ActivateInputField();
+        InputField.Select();
+    }
+
+    private void CommandsExecuting(string command, string originalMessage)
+    {
+        switch (command)
+        {
+            case "connect":
+
+                string[] SplitText = originalMessage.Split(new string[] {" " ,":" }, StringSplitOptions.None);
+
+                OutputText.text += "\n\r[" + DateTime.UtcNow + "] [Network] I'm starting the connection";
+
+                try
+                {
+
+                    if (SplitText[1] == null) {
+                        OutputText.text += $"\n\r[{ DateTime.UtcNow}] [Network] IP is missing, could not connect to server"; 
+                        return;
+                    }
+                    if (SplitText[2] == null) {
+                        OutputText.text += $"\n\r[{ DateTime.UtcNow}] [Network] Port is missing, could not connect to server"; 
+                        return;
+                    }
                     string Ip = SplitText[1];
                     int Port = int.Parse(SplitText[2]);
-
-                    print(Ip + ":" + Port);
+                    OutputText.text += $"\n\r[{ DateTime.UtcNow}] [Network] Connecnt to server // ip={Ip} //port={Port})";
 
                     bool status = Network.CreateConnect(Ip, Port);
 
@@ -54,9 +90,51 @@ public class Console : MonoBehaviour
                         OutputText.text += "\n\r[" + DateTime.UtcNow + "] [Network] Connected succesfully";
                     else
                         OutputText.text += "\n\r[" + DateTime.UtcNow + "] [Network] Connection could not be create";
+                }
+                catch
+                {
+                    OutputText.text += "\n\r[" + DateTime.UtcNow + "] [Network] Connection could not be create";
+                    return;
+                }
+            break;
+            case "ping_to_server":
 
-                    break;
-            }
+                OutputText.text += "\n\r[" + DateTime.UtcNow + "] [Network] I'm starting check ping to server";
+
+                //CosmoKotic - pay attention!
+                /*if(Network.IsConnectedToServer)
+                {
+                    OutputText.text += "\n\r[" + DateTime.UtcNow + "] [Network] To check the ping, you must be connected to the server!";
+                    return;
+                }
+                //int ping = Network.PingToServer();*/
+                int ping = 0;
+
+                OutputText.text +=  $"\n\r[{DateTime.UtcNow}] [Network] This ping to server = {ping}";
+
+                break;
+            case "my_position":
+                OutputText.text +=  $"\n\r[{DateTime.UtcNow}] [Local Game] This player position = {Network.Player.position}";
+
+                break;
+            
+            case "help":
+                OutputText.text +=  $"\n\r[{DateTime.UtcNow}] [Console] Commands: \n\rconnect ip:port\n\rping_to_server\n\rmy_position\n\rhelp";
+
+                break;
+            default:
+                OutputText.text += $"\n\r[{DateTime.UtcNow}] [Console] The command ({command}) is not correct";
+                break;
         }
+    }
+    public void OutputTextForward()
+    {
+        OutputText.pageToDisplay++;
+        OutputPageText.text = OutputText.pageToDisplay.ToString();
+    }
+    public void OutputTextBack()
+    {
+        OutputText.pageToDisplay--;
+        OutputPageText.text = OutputText.pageToDisplay.ToString();
     }
 }
