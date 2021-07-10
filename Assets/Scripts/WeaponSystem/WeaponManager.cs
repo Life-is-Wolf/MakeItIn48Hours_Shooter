2a0b56c156626227f845f3f9f53b43dfd01d71fa
Maxim360 <33699113+Maxim360Squad@users.noreply.github.com>
2021-07-10 03:37:36 +0300

    Fix name in BulletManager and WeaponManager

--- Assets/Scripts/WeaponSystem/WeaponManager.cs
+++ Assets/Scripts/WeaponSystem/WeaponManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponMenager : WeaponData
+public class WeaponManager : WeaponData
 {
     // Start is called before the first frame update
     void Start()
